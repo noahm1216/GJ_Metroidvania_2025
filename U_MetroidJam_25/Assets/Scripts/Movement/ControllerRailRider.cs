ef54b4ccffa58c63d9e2720c040c47eb3c59b472
Noah <16135831+noahm1216@users.noreply.github.com>
2025-11-18 23:43:55 -0800

    rail riding somewhat working

--- U_MetroidJam_25/Assets/Scripts/Movement/ControllerRailRider.cs
+++ U_MetroidJam_25/Assets/Scripts/Movement/ControllerRailRider.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Splines;
+
 
 [RequireComponent(typeof(Rigidbody))]
 public class ControllerRailRider : MonoBehaviour
 {
-    public enum MOTIONSTATE {NotRiding, Riding }
+    public enum MOTIONSTATE { NotRiding, Riding }
 
     // designer controlled numbers
     public float speedHorizontal = 350;
@@ -16,7 +20,16 @@ public class ControllerRailRider : MonoBehaviour
     public LayerMask jumpRestoreLayers;
     public LayerMask railWayLayers;
 
-    public float speedRailRide = 500;
+    [Header("Rail")]
+    [SerializeField] Rail currentRailScript;
+    public float railAcceleration = 2f;  // Optional acceleration along the rail
+    [SerializeField] float grindSpeed;
+    [SerializeField] float heightOffset;
+    float timeForFullSpline;
+    float elapsedTime;
+    [SerializeField] float lerpSpeed = 10f;
+    public MOTIONSTATE motionState;
+    public Spline splineRiding;
 
     // input mappings
     private string inputAxisHori = "Horizontal",
@@ -38,8 +51,8 @@ public class ControllerRailRider : MonoBehaviour
     private bool canJump;
     private bool jumpNow;
 
-    private MOTIONSTATE motionState;
-    public Vector3 dirEnteredRail;
+    
+
 
     // Start is called before the first frame update
     void Start()
@@ -51,7 +64,7 @@ public class ControllerRailRider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // move
+        // movement input while not riding
         movementInput = new Vector3(Input.GetAxis(inputAxisHori) * (1 + sprintAmount) // X
             , Input.GetAxis(inputAxisVert), // Y
             0); // Z
@@ -59,12 +72,11 @@ public class ControllerRailRider : MonoBehaviour
 
         switch (motionState)
         {
-            case MOTIONSTATE.NotRiding:           
-                // sprint
+            case MOTIONSTATE.NotRiding:
+                // Sprint logic
                 if (Mathf.Abs(Input.GetAxis(inputAxisSprint)) > deadzone)
                 {
-                    print($"pressing {inputAxisSprint}");
-                    if (!burstSprint) { sprintAmount = speedSprintMultiplier; } // first sprint from not sprinting
+                    if (!burstSprint) { sprintAmount = speedSprintMultiplier; }
 
                     sprintAmount -= (Time.deltaTime * sprintDecaySpeed);
 
@@ -73,55 +85,170 @@ public class ControllerRailRider : MonoBehaviour
                     burstSprint = true;
                 }
                 else
-                { burstSprint = false; sprintAmount = 0; }               
+                {
+                    burstSprint = false;
+                    sprintAmount = 0;
+                }
                 break;
+
             case MOTIONSTATE.Riding:
-                movementInput = dirEnteredRail;
+                // Move along the rail
+                if (splineRiding != null)
+                {
+                }
                 break;
+
             default:
                 break;
         }
 
-        // jump        
-        canJump = jumpsRemaining > 0 & Time.time > jumpTimeStamp + jumpTimeWait;
-        if (movementInput.y > 0 && canJump) { jumpNow = true; jumpTimeStamp = Time.time; jumpsRemaining--; }
-
+        // Jump logic
+        canJump = jumpsRemaining > 0 && Time.time > jumpTimeStamp + jumpTimeWait;
+        if (movementInput.y > 0 && canJump)
+        {
+            jumpNow = true;
+            jumpTimeStamp = Time.time;
+            jumpsRemaining--;
+        }
     }
-
 
     private void FixedUpdate()
     {
-        movementInput *= Time.deltaTime; // normalize against framerate
+        movementInput *= Time.deltaTime; // Normalize input against framerate
+
         if (motionState == MOTIONSTATE.NotRiding)
         {
-            rb3d.velocity = new Vector3(movementInput.x * speedHorizontal, // X
-                rb3d.velocity.y, // Y
-                0); // Z
+            rb3d.velocity = new Vector3(movementInput.x * speedHorizontal,  // X velocity
+                rb3d.velocity.y,  // Keep Y velocity the same
+                0);  // Z velocity
         }
         else
         {
-            rb3d.velocity = new Vector3(movementInput.x * speedRailRide, // X
-                rb3d.velocity.y, // Y
-                0); // Z
+            MovePlayerAlongRail();
         }
 
         if (jumpNow)
         {
             jumpNow = false;
-            rb3d.AddForce(((Vector3.up) * Time.deltaTime * speedJumpPower - rb3d.velocity), ForceMode.VelocityChange);
-            if (motionState == MOTIONSTATE.Riding) motionState = MOTIONSTATE.NotRiding;
+            if (motionState == MOTIONSTATE.Riding)
+                ThrowOffRail();
+            else
+                rb3d.AddForce(((Vector3.up) * Time.deltaTime * speedJumpPower - rb3d.velocity), ForceMode.VelocityChange);            
+        }
+    }
+
+    void MovePlayerAlongRail()
+    {
+        if (currentRailScript != null && motionState == MOTIONSTATE.Riding) //This is just some additional error checking.
+        {
+            //Calculate a 0 to 1 normalised time value which is the progress along the rail.
+            //Elapsed time divided by the full time needed to traverse the spline will give you that value.
+            float progress = elapsedTime / timeForFullSpline;
+
+            //If progress is less than 0, the player's position is before the start of the rail.
+            //If greater than 1, their position is after the end of the rail.
+            //In either case, the player has finished their grind.
+            if (progress < 0 || progress > 1)
+            {
+                ThrowOffRail();
+                return;
+            }
+            //The rest of this code will not execute if the player is thrown off.
+
+            //Next Time Normalised is the player's progress value for the next update.
+            //This is used for calculating the player's rotation.
+            //Depending on the direction of the player on the spline, it will either add or subtract time from the
+            //current elapsed time.
+            float nextTimeNormalised;
+            if (currentRailScript.normalDir)
+                nextTimeNormalised = (elapsedTime + Time.deltaTime) / timeForFullSpline;
+            else
+                nextTimeNormalised = (elapsedTime - Time.deltaTime) / timeForFullSpline;
+
+            //Calculating the local positions of the player's current position and next position
+            //using current progress and the progress for the next update.
+            float3 pos, tangent, up;
+            float3 nextPosfloat, nextTan, nextUp;
+            SplineUtility.Evaluate(currentRailScript.railSpline.Spline, progress, out pos, out tangent, out up);
+            SplineUtility.Evaluate(currentRailScript.railSpline.Spline, nextTimeNormalised, out nextPosfloat, out nextTan, out nextUp);
+
+            //Converting the local positions into world positions.
+            Vector3 worldPos = currentRailScript.LocalToWorldConversion(pos);
+            Vector3 nextPos = currentRailScript.LocalToWorldConversion(nextPosfloat);
+
+            //Setting the player's position and adding a height offset so that they're sitting on top of the rail
+            //instead of being in the middle of it.
+            transform.position = worldPos + (transform.up * heightOffset);
+            //Lerping the player's current rotation to the direction of where they are to where they're going.
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(nextPos - worldPos), lerpSpeed * Time.deltaTime);
+            //Lerping the player's up direction to match that of the rail, in relation to the player's current rotation.
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(transform.up, up) * transform.rotation, lerpSpeed * Time.deltaTime);
+
+            //Finally incrementing or decrementing elapsed time for the next update based on direction.
+            if (currentRailScript.normalDir)
+                elapsedTime += Time.deltaTime;
+            else
+                elapsedTime -= Time.deltaTime;
         }
     }
 
     private void OnCollisionEnter(Collision col)
     {
-        // Check if the otherLayer
+        // Reset jumps if collision with appropriate layer
         if (((1 << col.gameObject.layer) & jumpRestoreLayers) != 0 && col.transform.position.y < transform.position.y)
-        { jumpsRemaining = numberOfJump; } // print("reset jumps"); 
+        { jumpsRemaining = numberOfJump; }
 
+        // Check if collided with rail
         if (((1 << col.gameObject.layer) & railWayLayers) != 0 && col.transform.position.y < transform.position.y)
-        { print("hit RAIL"); dirEnteredRail = col.transform.position - transform.position; motionState = MOTIONSTATE.Riding; }
+        {
+            print("hit RAIL");            
+            motionState = MOTIONSTATE.Riding;
+
+            // Get SplineContainer and retrieve the spline
+            SplineContainer splineContainer = col.transform.GetComponent<SplineContainer>();
+            if (splineContainer)
+            {
+                currentRailScript = col.transform.GetComponent<Rail>();
+                splineRiding = splineContainer.Spline;
+                CalculateAndSetRailPosition();
+                rb3d.isKinematic = true;
+            }
+        }
+    }
+
+    void CalculateAndSetRailPosition()
+    {
+        //Figure out the amount of time it would take for the player to cover the rail.
+        timeForFullSpline = currentRailScript.totalSplineLength / grindSpeed;
+
+        //This is going to be the world position of where the player is going to start on the rail.
+        Vector3 splinePoint;
+
+        //The 0 to 1 value of the player's position on the spline. We also get the world position of where that
+        //point is.
+        float normalisedTime = currentRailScript.CalculateTargetRailPoint(transform.position, out splinePoint);
+        elapsedTime = timeForFullSpline * normalisedTime;
+        //Multiply the full time for the spline by the normalised time to get elapsed time. This will be used in
+        //the movement code.
+
+        //Spline evaluate takes the 0 to 1 normalised time above, 
+        //and uses it to give you a local position, a tangent (forward), and up
+        float3 pos, forward, up;
+        SplineUtility.Evaluate(currentRailScript.railSpline.Spline, normalisedTime, out pos, out forward, out up);
+        //Calculate the direction the player is going down the rail
+        currentRailScript.CalculateDirection(forward, transform.forward);
+        //Set player's initial position on the rail before starting the movement code.
+        transform.position = splinePoint + (transform.up * heightOffset);
+    }
+
+
+    void ThrowOffRail()
+    {
+        //Set onRail to false, clear the rail script, and push the player off the rail.
+        //It's a little sudden, there might be a better way of doing using coroutines and looping, but this will work.
+        motionState = MOTIONSTATE.NotRiding;
+        currentRailScript = null;
+        transform.position += transform.forward * 1;
+        rb3d.isKinematic = false;
     }
 }
-
-
